7e69abc7c7b8fa7aa50fdc456c0db4bc37952400
Sumanthc18 <132780398+Sumanthc18@users.noreply.github.com>
2023-10-08 00:38:43 +0200

    Update geo_location.py

--- homeassistant/components/ign_sismologia/geo_location.py
+++ homeassistant/components/ign_sismologia/geo_location.py
@@ -26,7 +26,7 @@ import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.dispatcher import async_dispatcher_connect, dispatcher_send
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.event import track_time_interval
-from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
+from homeassistant.helpers.typing import ConfigType
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -61,8 +61,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
 def setup_platform(
     hass: HomeAssistant,
     config: ConfigType,
-    add_entities: AddEntitiesCallback,
-    discovery_info: DiscoveryInfoType | None = None,
+    add_entities: AddEntitiesCallback
 ) -> None:
     """Set up the IGN Sismologia Feed platform."""
     scan_interval: timedelta = config.get(CONF_SCAN_INTERVAL, SCAN_INTERVAL)
