1868d64c044e24a2becd53b07073a340e677c144
Sumanthc18 <132780398+Sumanthc18@users.noreply.github.com>
2023-10-08 01:12:14 +0200

    Update __init__.py

--- homeassistant/components/imap_email_content/__init__.py
+++ homeassistant/components/imap_email_content/__init__.py
@@ -1,7 +1,6 @@
 """The imap_email_content component."""
 
 from homeassistant.const import Platform
-from homeassistant.core import HomeAssistant
 from homeassistant.helpers import config_validation as cv
 
 from .const import DOMAIN
@@ -11,6 +10,6 @@ PLATFORMS = [Platform.SENSOR]
 CONFIG_SCHEMA = cv.deprecated(DOMAIN, raise_if_present=False)
 
 
-async def async_setup(hass: HomeAssistant) -> bool:
+async def async_setup() -> bool:
     """Set up imap_email_content."""
     return True
