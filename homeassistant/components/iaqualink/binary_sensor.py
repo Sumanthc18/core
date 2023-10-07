6cf4bd9d80d80faa8193ad1d391443adea6ee9ee
Sumanthc18 <132780398+Sumanthc18@users.noreply.github.com>
2023-10-07 23:48:57 +0200

    Update binary_sensor.py

--- homeassistant/components/iaqualink/binary_sensor.py
+++ homeassistant/components/iaqualink/binary_sensor.py
@@ -20,7 +20,6 @@ PARALLEL_UPDATES = 0
 
 async def async_setup_entry(
     hass: HomeAssistant,
-    config_entry: ConfigEntry,
     async_add_entities: AddEntitiesCallback,
 ) -> None:
     """Set up discovered binary sensors."""
