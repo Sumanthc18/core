078f30d414bd95e0ba3eb2a3041cb49418d10ef1
Sumanthc18 <132780398+Sumanthc18@users.noreply.github.com>
2023-10-08 03:50:37 +0200

    Update alarm_control_panel.py

--- homeassistant/components/lupusec/alarm_control_panel.py
+++ homeassistant/components/lupusec/alarm_control_panel.py
@@ -15,7 +15,7 @@ from homeassistant.const import (
 )
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
+from homeassistant.helpers.typing import DiscoveryInfoType
 
 from . import DOMAIN as LUPUSEC_DOMAIN, LupusecDevice
 
@@ -24,7 +24,6 @@ SCAN_INTERVAL = timedelta(seconds=2)
 
 def setup_platform(
     hass: HomeAssistant,
-    config: ConfigType,
     add_entities: AddEntitiesCallback,
     discovery_info: DiscoveryInfoType | None = None,
 ) -> None:
