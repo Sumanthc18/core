6673154100f4b25410517976f7d8b8371fda429b
Sumanthc18 <132780398+Sumanthc18@users.noreply.github.com>
2023-10-08 22:26:31 +0200

    Update sensor.py

--- homeassistant/components/mfi/sensor.py
+++ homeassistant/components/mfi/sensor.py
@@ -26,7 +26,7 @@ from homeassistant.const import (
 from homeassistant.core import HomeAssistant
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
+from homeassistant.helpers.typing import ConfigType
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -60,7 +60,6 @@ def setup_platform(
     hass: HomeAssistant,
     config: ConfigType,
     add_entities: AddEntitiesCallback,
-    discovery_info: DiscoveryInfoType | None = None,
 ) -> None:
     """Set up mFi sensors."""
     host = config.get(CONF_HOST)
