f5b50937b8da7a62f0d8d743e657dd218630b880
Sumanthc18 <132780398+Sumanthc18@users.noreply.github.com>
2023-10-08 03:55:34 +0200

    Update cover.py

--- homeassistant/components/lutron/cover.py
+++ homeassistant/components/lutron/cover.py
@@ -11,7 +11,7 @@ from homeassistant.components.cover import (
 )
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
+from homeassistant.helpers.typing import DiscoveryInfoType
 
 from . import LUTRON_CONTROLLER, LUTRON_DEVICES, LutronDevice
 
@@ -20,7 +20,6 @@ _LOGGER = logging.getLogger(__name__)
 
 def setup_platform(
     hass: HomeAssistant,
-    config: ConfigType,
     add_entities: AddEntitiesCallback,
     discovery_info: DiscoveryInfoType | None = None,
 ) -> None:
