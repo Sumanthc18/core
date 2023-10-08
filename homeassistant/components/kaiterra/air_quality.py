f295e633ebab994a3af60f89d6f3c25e6b6b40d2
Sumanthc18 <132780398+Sumanthc18@users.noreply.github.com>
2023-10-08 03:10:54 +0200

    Update air_quality.py

--- homeassistant/components/kaiterra/air_quality.py
+++ homeassistant/components/kaiterra/air_quality.py
@@ -6,7 +6,7 @@ from homeassistant.const import CONF_DEVICE_ID, CONF_NAME
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.dispatcher import async_dispatcher_connect
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
+from homeassistant.helpers.typing import DiscoveryInfoType
 
 from .const import (
     ATTR_AQI_LEVEL,
@@ -19,7 +19,6 @@ from .const import (
 
 async def async_setup_platform(
     hass: HomeAssistant,
-    config: ConfigType,
     async_add_entities: AddEntitiesCallback,
     discovery_info: DiscoveryInfoType | None = None,
 ) -> None:
