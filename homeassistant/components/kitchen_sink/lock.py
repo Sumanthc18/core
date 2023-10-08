b97a0641482696cf93dc23502b4754a06a7a3db7
Sumanthc18 <132780398+Sumanthc18@users.noreply.github.com>
2023-10-08 03:31:05 +0200

    Update lock.py

--- homeassistant/components/kitchen_sink/lock.py
+++ homeassistant/components/kitchen_sink/lock.py
@@ -12,7 +12,6 @@ from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 
 
 async def async_setup_platform(
-    hass: HomeAssistant,
     config: ConfigType,
     async_add_entities: AddEntitiesCallback,
     discovery_info: DiscoveryInfoType | None = None,
