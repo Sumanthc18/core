e5d3d8f5bb07418efc90dd7f6ca6a5040c03f471
Sumanthc18 <sumanthc18@gmail.com>
2023-10-08 02:04:51 +0200

    Update __init__.py

--- homeassistant/components/insteon/__init__.py
+++ homeassistant/components/insteon/__init__.py
@@ -10,7 +10,6 @@ from homeassistant.const import CONF_PLATFORM, EVENT_HOMEASSISTANT_STOP
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryNotReady
 from homeassistant.helpers import config_validation as cv, device_registry as dr
-from homeassistant.helpers.typing import ConfigType
 
 from . import api
 from .const import (
@@ -37,7 +36,7 @@ OPTIONS = "options"
 CONFIG_SCHEMA = cv.removed(DOMAIN, raise_if_present=False)
 
 
-async def async_get_device_config(hass, config_entry):
+async def async_get_device_config(hass):
     """Initiate the connection and services."""
     # Make a copy of addresses due to edge case where the list of devices could
     # change during status update
@@ -75,7 +74,7 @@ async def close_insteon_connection(*args):
     await async_close()
 
 
-async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+async def async_setup() -> bool:
     """Set up the Insteon platform."""
     return True
 
