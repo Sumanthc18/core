75d92765f32dcc691080c7f0129e5010261c492c
Sumanthc18 <132780398+Sumanthc18@users.noreply.github.com>
2023-10-08 03:05:17 +0200

    Update sensor.py

--- homeassistant/components/jewish_calendar/sensor.py
+++ homeassistant/components/jewish_calendar/sensor.py
@@ -17,7 +17,7 @@ from homeassistant.const import SUN_EVENT_SUNSET
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.sun import get_astral_event_date
-from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
+from homeassistant.helpers.typing import DiscoveryInfoType
 import homeassistant.util.dt as dt_util
 
 from . import DOMAIN
@@ -143,7 +143,6 @@ TIME_SENSORS = (
 
 async def async_setup_platform(
     hass: HomeAssistant,
-    config: ConfigType,
     async_add_entities: AddEntitiesCallback,
     discovery_info: DiscoveryInfoType | None = None,
 ) -> None:
