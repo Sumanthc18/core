e46fd05d3efe165c3763ce6f820030f6c9481551
Sumanthc18 <132780398+Sumanthc18@users.noreply.github.com>
2023-10-08 03:15:18 +0200

    Update binary_sensor.py

--- homeassistant/components/keba/binary_sensor.py
+++ homeassistant/components/keba/binary_sensor.py
@@ -9,14 +9,13 @@ from homeassistant.components.binary_sensor import (
 )
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
+from homeassistant.helpers.typing import DiscoveryInfoType
 
 from . import DOMAIN, KebaHandler
 
 
 async def async_setup_platform(
     hass: HomeAssistant,
-    config: ConfigType,
     async_add_entities: AddEntitiesCallback,
     discovery_info: DiscoveryInfoType | None = None,
 ) -> None:
